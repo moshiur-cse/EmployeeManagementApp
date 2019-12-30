5406f9e1295e70ff13d4176c83f32bdbe206e9cc
moshiur-cse <Rimu@1234>
2019-12-30 19:41:36 +0600

    Add Class

---
+++ app/src/main/java/com/moshiurcse/employeemanagement/models/BasePlusCommisionEmployee.java
@@ -0,0 +1,22 @@
+package com.moshiurcse.employeemanagement.models;
+
+public class BasePlusCommisionEmployee {
+    private double commision_rate;
+    private double gross_total;
+
+    public double getCommision_rate() {
+        return commision_rate;
+    }
+
+    public void setCommision_rate(double commision_rate) {
+        this.commision_rate = commision_rate;
+    }
+
+    public double getGross_total() {
+        return gross_total;
+    }
+
+    public void setGross_total(double gross_total) {
+        this.gross_total = gross_total;
+    }
+}
---
+++ app/src/main/java/com/moshiurcse/employeemanagement/models/HourlySalarriedEmployee.java
@@ -0,0 +1,22 @@
+package com.moshiurcse.employeemanagement.models;
+
+public class HourlySalarriedEmployee extends Employee {
+    private double hourly_rate;
+    private double total_hour;
+
+    public double getHourly_rate() {
+        return hourly_rate;
+    }
+
+    public void setHourly_rate(double hourly_rate) {
+        this.hourly_rate = hourly_rate;
+    }
+
+    public double getTotal_hour() {
+        return total_hour;
+    }
+
+    public void setTotal_hour(double total_hour) {
+        this.total_hour = total_hour;
+    }
+}
