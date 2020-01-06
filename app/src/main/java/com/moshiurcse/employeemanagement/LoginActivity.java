1b783855f16cef723c8b9048b0c7d14987f0ec7c
moshiur-cse <Rimu@1234>
2020-01-06 19:05:59 +0600

    TextInputEditText and Layout

---
+++ app/src/main/java/com/moshiurcse/employeemanagement/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.moshiurcse.employeemanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+     private TextInputEditText emailET,passwordET;
+     private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        emailET=findViewById(R.id.emailET);
+        passwordET=findViewById(R.id.passwordET);
+
+    }
+
+    public void loginAdmin(View view) {
+        String email=emailET.getText().toString();
+        String pass=passwordET.getText().toString();
+    }
+}
