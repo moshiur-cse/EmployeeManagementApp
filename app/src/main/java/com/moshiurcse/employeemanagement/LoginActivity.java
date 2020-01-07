943d30268dd42682eaf2bcbb309bf48ad83d6f49
Moshiur Rahman <moshiur_cse@hotmail.com>
2020-01-07 10:28:42 +0600

    Update Login Activity

--- app/src/main/java/com/moshiurcse/employeemanagement/LoginActivity.java
+++ app/src/main/java/com/moshiurcse/employeemanagement/LoginActivity.java
@@ -2,30 +2,43 @@ package com.moshiurcse.employeemanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.moshiurcse.employeemanagement.utils.ConstantUtils;
 
 public class LoginActivity extends AppCompatActivity {
 
-     private TextInputEditText emailET,passwordET;
-     private Button loginBtn;
+    TextInputEditText mailAdressET,passwordET;
+    Button loginAdminBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        emailET=findViewById(R.id.emailET);
+        mailAdressET=findViewById(R.id.mailAdressET);
         passwordET=findViewById(R.id.passwordET);
 
     }
 
-    public void loginAdmin(View view) {
-        String email=emailET.getText().toString();
-        String pass=passwordET.getText().toString();
+    public void loginAdminBtn(View view) {
+
+        String mailAdress=mailAdressET.getText().toString();
+        String password=passwordET.getText().toString();
+
+        if (mailAdress.equals(ConstantUtils.Admin.EMAIL_ADRESS) && password.equals(ConstantUtils.Admin.PASSWORD)){
+            finish();
+            Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+            startActivity(intent);
+
+        }else{
+            Toast.makeText(this,"Log in fails",Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/moshiurcse/employeemanagement/utils/ConstantUtils.java
@@ -0,0 +1,8 @@
+package com.moshiurcse.employeemanagement.utils;
+
+public class ConstantUtils {
+    public static class Admin{
+        public static final String EMAIL_ADRESS="admin@cegisbd.com";
+        public static final String PASSWORD="123456";
+    }
+}
